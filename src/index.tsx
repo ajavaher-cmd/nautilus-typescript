76e12b7a0c1932f6e676dd19bb1c3b47f276a392
ajavaher <ajavaher@ualberta.ca>
2022-12-26 09:40:28 -0700

    github ribbon

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,11 @@ import Calculator from "./Calculator";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./index.css";
+import GitHubRibbon from 'react-github-ribbons';
 
 ReactDOM.render(
   <Provider store={store}>
+    <GitHubRibbon href="https://github.com/ajavaher-cmd/nautilus-typescript" />
     <h1 className="h1">Nautilus</h1>
     <Calculator />
   </Provider>
